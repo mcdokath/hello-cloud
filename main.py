4f13257e6dd4b1e643a4617573352988c2ccc4f1
mcdokath <mcdokath@users.noreply.github.com>
2016-03-31 21:44:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,24 @@
 
 import webapp2
 
+MAIN_PAGE_HTML = """\
+<!DOCTYPE html>
+<html>
+<head>
+  <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  <title>Hello Cloud</title>
+</head>
+<body>
+  <h1>Hello, Cloud!</h1>
+  <p>This is a sample webpage.</p>
+</body>
+</html>
+"""
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = "text/plain"
-		self.response.write("Hello, World!")
+		self.response.write(MAIN_PAGE_HTML)
 
 app = webapp2.WSGIApplication([
 	("/", MainPage),
