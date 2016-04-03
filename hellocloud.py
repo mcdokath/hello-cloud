16192e88b37661b9b6285a7d6b402ef38390969f
mcdokath <mcdokath@users.noreply.github.com>
2016-04-03 12:03:46 -0700

    Update hellocloud.py

--- hellocloud.py
+++ hellocloud.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template 
+from jinja2 import Environment, PackageLoader
 
-app = Flask(__name__) 
-@app.route('/')
-
-def home():
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+env = Environment(loader=PackageLoader("hello-cloud", "templates"))
+template = env.get_template("index.html")
