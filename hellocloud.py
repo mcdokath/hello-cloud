e6f378b8ae928b1bacf7a8ff414daf8f190b24d8
mcdokath <mcdokath@users.noreply.github.com>
2016-04-03 12:30:09 -0700

    Update hellocloud.py

--- hellocloud.py
+++ hellocloud.py
@@ -1,10 +1,18 @@
 import time
 import jinja2
+import webapp2
 
-# create output string
-output = "Current time: {{ timeobj.strftime('%H:%M') }}"
-print(output)
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    
+    # create output string
+    output = "Current time: {{ timeobj.strftime('%H:%M') }}"
 
-# put output string into jinja
-jOutput = jinja2.Template(output)
-print(jOutput.render(timeobj = time))
+    # put output string into jinja
+    jOutput = jinja2.Template(output)
+    self.response.write(jOutput.render(timeobj = time))
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage),
+], debug=True)
