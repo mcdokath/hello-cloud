2bfed8090589947de5eb42fb9ea00328fad038e9
mcdokath <mcdokath@users.noreply.github.com>
2016-04-03 12:49:26 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,11 @@
+import time
+import jinja2
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Hello Cloud!')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
